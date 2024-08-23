0cc25ee16bb15d70b2ad3b68ee54eb1f1105ec66
ms-tech-geek <thecorporator@gmail.com>
2024-08-23 19:15:40 +0530

    building and using a reusable input component

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,81 +1,78 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Input from "./Input";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: '',
-  });
+	const [enteredValues, setEnteredValues] = useState({
+		email: "",
+		password: "",
+	});
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
+	const [didEdit, setDidEdit] = useState({
+		email: false,
+		password: false,
+	});
 
+	const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const passwordIsInvalid = didEdit.password && !enteredValues.password.trim().length<6;
 
+	function handleSubmit(event) {
+		event.preventDefault();
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+		console.log(enteredValues);
+	}
 
-  function handleSubmit(event) {
-    event.preventDefault();
+	function handleInputChange(identifier, value) {
+		setEnteredValues((prevValues) => ({
+			...prevValues,
+			[identifier]: value,
+		}));
+		setDidEdit((prevEdit) => ({
+			...prevEdit,
+			[identifier]: false,
+		}));
+	}
 
-    console.log(enteredValues);
-  }
+	function handleInputBlur(identifier) {
+		setDidEdit((prevEdit) => ({
+			...prevEdit,
+			[identifier]: true,
+		}));
+	}
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2>Login</h2>
 
-  function handleInputBlur(identifier) {
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: true,
-    }));
-  }
+			<div className="control-row">
+				<Input
+					label="Email"
+					id="email"
+					type="email"
+					name="email"
+					onBlur={() => handleInputBlur("email")}
+					onChange={(event) => handleInputChange("email", event.target.value)}
+					value={enteredValues.email}
+          error={emailIsInvalid && 'Please enter a valid email address'}
+				/>
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+				<Input
+					label="Password"
+					id="password"
+					type="password"
+					name="password"
+					onBlur={() => handleInputBlur("password")}
+					onChange={(event) =>
+						handleInputChange("password", event.target.value)
+					}
+					value={enteredValues.password}
+          error={passwordIsInvalid && 'Please enter a valid password'}
+				/>
+			</div>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onBlur={()=>handleInputBlur('email')}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-            value={enteredValues.email}
-          />
-          <div className='control-error'>{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onBlur={()=>handleInputBlur('password')}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-            value={enteredValues.password}
-          />
-        </div>
-      </div>
-
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+			<p className="form-actions">
+				<button className="button button-flat">Reset</button>
+				<button className="button">Login</button>
+			</p>
+		</form>
+	);
 }
